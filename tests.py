dfd5362bee1c8d3d1536148093806b1e1e4e855d
Miles Elam <miles@geekspeak.org>
2017-06-17 20:24:59 -0700

    Updated tests to include qual checks

--- tests.py
+++ tests.py
@@ -4,6 +4,13 @@ from unittest import TestCase, main
 from unittest.mock import patch, MagicMock
 from io import StringIO
 
+class Qual:
+  def __init__(self, name, op, value, is_list=False):
+    self.field_name = name
+    self.operator = op
+    self.value = value
+    self.is_list_operator = is_list
+
 class ForeignDataWrapper:
   def __init__(self, options, columns):
     pass
@@ -21,7 +28,7 @@ ID_PATTERN = 1
 ID_EXECUTABLE = 2
 
 TEST_DIR = '/usr/share/example/'
-TEST_ARGS = ['/usr/bin/find', '-O3', '-ignore_readdir_race', TEST_DIR]
+TEST_ARGS = ['/usr/bin/find', '-O3', TEST_DIR, '-ignore_readdir_race']
 
 class FindWrapperTests(TestCase):
 
@@ -53,7 +60,7 @@ class FindWrapperTests(TestCase):
     row = next(fw.execute([], columns))
     expected = TEST_ARGS + [ '-printf', '%P\t%T+\n' ]
     # self.assertTrue(mock_popen.called)
-    mock_popen.assert_called_with(expected, stdout=mock_pipe)
+    mock_popen.assert_called_with(expected, stdout=mock_pipe, universal_newlines=True)
     self.assertEqual(row, { 'modified': '2017-03-22 22:33:15.3646792370', 'path': 'example.txt' })
 
   @patch('ttfkam.Popen')
@@ -89,7 +96,7 @@ class FindWrapperTests(TestCase):
                             'mime_type': 'text/plain' })
     expected = TEST_ARGS + [ '-printf', '%P\t%T+\n',
                              '-exec', '/usr/bin/file', '-b', '-i', '{}', ';' ]
-    mock_popen.assert_called_with(expected, stdout=mock_pipe)
+    mock_popen.assert_called_with(expected, stdout=mock_pipe, universal_newlines=True)
 
   @patch('ttfkam.Popen')
   @patch('ttfkam.PIPE')
@@ -105,7 +112,21 @@ class FindWrapperTests(TestCase):
     self.assertEqual(row, { 'ts': '2017-03-22 22:33:15.3646792370',
                             'path': 'example.txt' })
     expected = TEST_ARGS + [ '-printf', '%P\t%T+\n' ]
-    mock_popen.assert_called_with(expected, stdout=mock_pipe)
+    mock_popen.assert_called_with(expected, stdout=mock_pipe, universal_newlines=True)
+
+  @patch('ttfkam.Popen')
+  @patch('ttfkam.PIPE')
+  def test_quals(self, mock_pipe, mock_popen):
+    process_mock = MagicMock()
+    process_attrs = { 'stdout': StringIO('example.txt\n') }
+    process_mock.configure_mock(**process_attrs)
+    mock_popen.return_value = process_mock
+    columns = { 'path': None, 'depth': None, 'type': None }
+    fw = FindWrapper({ 'root_directory': TEST_DIR }, columns)
+    row = next(fw.execute([Qual('depth','=',1), Qual('type','=','f')], { 'path': None }))
+    self.assertEqual(row, { 'path': 'example.txt' })
+    expected = TEST_ARGS + [ '-depth', '1', '-type', 'f', '-printf', '%P\n' ]
+    mock_popen.assert_called_with(expected, stdout=mock_pipe, universal_newlines=True)
 
 if __name__ == '__main__':
   main()
