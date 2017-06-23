bf11f4cff70e97267ea543eda0d9bbc07a7076a1
Miles Elam <miles@geekspeak.org>
2017-06-23 12:07:08 -0700

    Not an extension anymore. Just considered a pure python module.

--- find--1.0.0.sql
+++
@@ -1,9 +0,0 @@
--- ===========================================================================
--- GNU find PostgreSQL extension
--- Miles Elam <miles@geekspeak.org>
---
--- Depends on multicorn
--- ---------------------------------------------------------------------------
-
--- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "CREATE EXTENSION gnufind" to load this file. \quit
--- setup.py
+++ setup.py
@@ -1,29 +1,15 @@
 #!/usr/bin/env python3
 
-import subprocess
 import sys
-from setuptools import setup, find_packages, Extension
-
-# hum... borrowed from psycopg2
-def get_pg_config(kind, pg_config="pg_config"):
-    p = subprocess.Popen([pg_config, '--%s' % kind], stdout=subprocess.PIPE)
-    r = p.communicate()
-    r = r[0].strip().decode('utf8')
-    if not r:
-        raise Warning(p[2].readline())
-    return r
-
-include_dirs = [get_pg_config(d) for d in ("includedir", "pkgincludedir", "includedir-server")]
-
-requires=[]
+from setuptools import setup
 
 if sys.version_info[0] < 3 or sys.version_info[1] < 3:
     sys.exit("Sorry, you need at least python 3.3 for pg_gnufind")
 
 setup(
-    name='multicorn',
+    name='ttfkam',
     version='__VERSION__',
-    author='Kozea',
+    author='ttfkam',
     license='GPL2',
     package_dir={'': 'python'},
     packages=[],
